54c911c676b083d0e09f53d3bca1c6012dd408f8
HaxMirage <USERNAME@example.com>
2020-09-07 16:47:59 -0400

    functional filter by country

--- js/content_utility/search_bar.js
+++ js/content_utility/search_bar.js
@@ -133,6 +133,7 @@ function updateAvailableCountries(){
 	  var icon = document.createElement("i");
 	  icon.className = "flagico f-" + code;
 	  list.innerHTML = code.charAt(0).toUpperCase() + code.slice(1);
+	  list.innerText = code;
 	  list.onclick = selectedListElement;
 	  anchor.dataset.target = code.charAt(0).toUpperCase() + code.slice(1);
 	  anchor.appendChild(icon);
@@ -144,5 +145,44 @@ function updateAvailableCountries(){
 }
 
 function selectedListElement() {
-    console.log(this.innerHTML)
+	var countryCode = this.innerText;
+	filterByCountry(countryCode);
+	
+}
+
+function filterByCountry(requestedCountryCode){
+	var gameframe = document.getElementsByClassName("gameframe")[0];
+	var dialog = gameframe.contentDocument.getElementsByClassName("dialog")[0];
+	var input = gameframe.contentWindow.document.getElementById('searchRoom');
+    var searchRoom = input.value.toLowerCase();
+	chrome.storage.local.set({'haxRoomSearchTerm': input.value}, function (obj) { });
+    var roomTable = dialog.querySelectorAll("[data-hook='list']")[0]
+    var totalNumberOfPlayers = 0;
+	var totalNumberOfRooms = 0;
+
+    for(room of roomTable.rows) {
+        var roomName = room.querySelectorAll("[data-hook='name']")[0].innerText;
+        var roomNumPlayers = room.querySelectorAll("[data-hook='players']")[0].innerText.split('/')[0];
+		var roomMaxPlayers = room.querySelectorAll("[data-hook='players']")[0].innerText.split('/')[1];
+		var roomFlag = room.querySelectorAll("[data-hook='flag']")[0];
+        var roomName = roomName.toLowerCase();
+		var rexp = /([^\/]+)?\/?(\d+)?/.exec(searchRoom)
+		
+		var playerTest = (typeof(rexp[2]) === 'undefined' || rexp[2] == roomMaxPlayers);
+		var searchTerms = rexp[1] ? rexp[1].split('+').filter(x => x != '') : [];
+		var countryCode = roomFlag.getAttribute("class").replace('flagico f-','');
+		function myIncl(roomName, terms) {
+			return terms.split(' ').every(x => roomName.includes(x));
+		}
+		
+		if ((searchTerms.some(x => myIncl(roomName, x) || myIncl(roomName.replace(/\s/g,''), x)) || !searchTerms.length) && playerTest && requestedCountryCode == countryCode) {
+			room.hidden = false;
+			totalNumberOfPlayers += parseInt(roomNumPlayers);
+			totalNumberOfRooms++;
+        }
+    else { room.hidden = true; }
+    }
+    var roomsStats = dialog.querySelectorAll("[data-hook='count']")[0];
+    roomsStats.innerText = totalNumberOfPlayers + " players in "+totalNumberOfRooms+" filtered rooms";
+    dialog.querySelector("[data-hook='listscroll']").scrollTo(0,0);
 }
