978755ed6c66c0f8d4c7a78f08a4c9d42b066c0e
HaxMirage <USERNAME@example.com>
2020-09-08 21:19:09 -0400

    search country functionality works

--- js/content_utility/search_bar.js
+++ js/content_utility/search_bar.js
@@ -17,11 +17,6 @@ function createSearch(){
 	var input = document.createElement('input'); 
 	input.type = "search"; 
 	input.id = "searchRoom";
-	chrome.storage.local.get({'haxRoomSearchTerm': ''}, function(result) {
-		input.value = result.haxRoomSearchTerm;
-		refreshButton.click();
-	});
-	
 	input.placeholder = "Term1 Term2+Term3/RoomMax - Search bar by Raamyy and xenon";
 	input.autocomplete = "off";
 	input.style.width = "75%";
@@ -43,11 +38,17 @@ function createSearch(){
 	searchExample.innerText = 'Search example: Hax Ball+pro/14 finds rooms with Hax and Ball, OR pro (max players 14)';
 
 	var button = document.createElement("BUTTON");
-	button.innerHTML = "Filter By Country";
+	button.innerHTML = "Select Country";
 	button.id = "searchRoomByCountry"
 	button.className = "dropbtn";
 	button.className =  "dropdown";
 	button.style.width = "25%";
+	
+	chrome.storage.local.get({'haxRoomSearchTerm': '', 'haxRoomCountrySearchTerm': 'All'}, function(result) {
+		input.value = result.haxRoomSearchTerm;
+		button.value = result.haxRoomCountrySearchTerm;
+		refreshButton.click();
+	});
 
 	var style = document.createElement('link');
 	style.rel = 'stylesheet';
@@ -70,13 +71,17 @@ function searchForRoom() {
 	var gameframe = document.getElementsByClassName("gameframe")[0];
 	var dialog = gameframe.contentDocument.getElementsByClassName("dialog")[0];
 	var input = gameframe.contentWindow.document.getElementById('searchRoom');
+
+	if (gameframe.contentWindow.document.getElementById('searchRoomByCountry').value) {
+		var requestedCountryCode = gameframe.contentWindow.document.getElementById('searchRoomByCountry').value
+	}
+	else {
+		var requestedCountryCode = 'All';
+	}
+	
 	var searchRoom = input.value.toLowerCase();
 	chrome.storage.local.set({'haxRoomSearchTerm': input.value}, function (obj) { });
-	var allCountries = "All";
-	var requestedCountryCode = window.localStorage.getItem('haxRoomCountrySearchTerm');
-	/*var requestedCountryCode = allCountries;
-	requestedCountryCode = await getLocalStorageValue('haxRoomCountrySearchTerm');
-	console.log(requestedCountryCode);*/
+
     var roomTable = dialog.querySelectorAll("[data-hook='list']")[0]
     var totalNumberOfPlayers = 0;
 	var totalNumberOfRooms = 0;
@@ -85,23 +90,18 @@ function searchForRoom() {
         var roomName = room.querySelectorAll("[data-hook='name']")[0].innerText;
         var roomNumPlayers = room.querySelectorAll("[data-hook='players']")[0].innerText.split('/')[0];
 		var roomMaxPlayers = room.querySelectorAll("[data-hook='players']")[0].innerText.split('/')[1];
-		var roomFlag = room.querySelectorAll("[data-hook='flag']")[0];
+		var countryCode = room.querySelectorAll("[data-hook='flag']")[0].className.replace('flagico f-','');
+		var countryCode = countryCode === '' ? 'eu' : countryCode;
         var roomName = roomName.toLowerCase();
 		var rexp = /([^\/]+)?\/?(\d+)?/.exec(searchRoom)
 		
 		var playerTest = (typeof(rexp[2]) === 'undefined' || rexp[2] == roomMaxPlayers);
 		var searchTerms = rexp[1] ? rexp[1].split('+').filter(x => x != '') : [];
-		var countryCode = roomFlag.getAttribute("class").replace('flagico f-','');
 		function myIncl(roomName, terms) {
 			return terms.split(' ').every(x => roomName.includes(x));
 		}
 
-		if(requestedCountryCode == allCountries){
-			countryCode = allCountries;
-		}
-
-		if ((searchTerms.some(x => myIncl(roomName, x) || myIncl(roomName.replace(/\s/g,''), x)) || !searchTerms.length) && playerTest 
-			&& countryCode == requestedCountryCode) {
+		if ((searchTerms.some(x => myIncl(roomName, x) || myIncl(roomName.replace(/\s/g,''), x)) || !searchTerms.length) && playerTest && (requestedCountryCode === countryCode || requestedCountryCode === 'All')) {
 			room.hidden = false;
 			totalNumberOfPlayers += parseInt(roomNumPlayers);
 			totalNumberOfRooms++;
@@ -115,9 +115,9 @@ function searchForRoom() {
     dialog.querySelector("[data-hook='listscroll']").scrollTo(0,0);
 }
 
-var gameframe = document.getElementsByClassName("gameframe")[0];
 
 function updateAvailableCountries(){
+	var gameframe = document.getElementsByClassName("gameframe")[0];
 	var dialog = gameframe.contentDocument.getElementsByClassName("dialog")[0];
 	var flags = dialog.querySelectorAll("[data-hook='flag']");
 	var uniqueFlags = new Set();
@@ -140,12 +140,13 @@ function updateAvailableCountries(){
 	unorderedList.appendChild(allCountriesList);
 
 	for (var code of countryCodes) {
-		if(code.length > 0){
+		if(code.length != 0){
 			var list = document.createElement("li");
 			var anchor = document.createElement("a");
 			var icon = document.createElement("i");
 			icon.className = "flagico f-" + code;
 			list.id = "searchListByCountry";
+			anchor.id = 'selectedCountry';
 			anchor.text = code;
 			anchor.onclick = selectedAnchorElement;
 			anchor.dataset.target = code.charAt(0).toUpperCase() + code.slice(1);
@@ -153,6 +154,7 @@ function updateAvailableCountries(){
 			list.appendChild(anchor);
 			unorderedList.appendChild(list);
 		}
+		
 	}
 	dropDownDiv.appendChild(unorderedList);
 	button.appendChild(dropDownDiv);
@@ -160,10 +162,12 @@ function updateAvailableCountries(){
 
 function selectedAnchorElement() {
 	var countryCode = this.text;
-	window.localStorage.setItem('haxRoomCountrySearchTerm', countryCode);
-	//chrome.storage.local.set({'haxRoomCountrySearchTerm': countryCode}, function (obj) { });
-	searchForRoom();
-
+	// window.localStorage.setItem('haxRoomCountrySearchTerm', countryCode);
+	chrome.storage.local.set({'haxRoomCountrySearchTerm': countryCode}, function (obj) { 
+		var gameframe = document.getElementsByClassName("gameframe")[0];
+		gameframe.contentWindow.document.getElementById("searchRoomByCountry").value = countryCode;
+		searchForRoom();
+	});
 }
 
 
